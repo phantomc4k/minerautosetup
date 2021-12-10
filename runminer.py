cdfcfbbe8882949b9c477bde1157d81d5de756c4
phantomc4k <69833877+phantomc4k@users.noreply.github.com>
2021-12-09 21:08:12 -0600

    Add files via upload

--- runminer.py
+++ runminer.py
@@ -8,6 +8,7 @@ p = subprocess.Popen("hostname -I", stdout=subprocess.PIPE, shell=True)
 (output1, err) = p.communicate()
 output1=str(output1)
 name= output1.split(".")[3]
+name=name[0:3]
 print("hostnamectl set-hostname miner"+name)
 #os.system("hostnamectl set-hostname miner"+name)
 
@@ -16,5 +17,5 @@ while True:
     p = subprocess.Popen("ps -A", stdout=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
     output=str(output)
-    if len(output.split("xmrig"))<1:
+    if len(output.split("xmrig"))<=1:
         os.system("curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 42Xk3DR9p73PGrKfNC7NGzeWRwJMqXovuDTtKHss8XWJAKHPJ1ZV779E26qGi3mukeD9emegL1QpZJ7B2nYJPKMvK1UxWMe")
